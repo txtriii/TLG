b7c5183485b1c9a7d750cc33ed3e8fc03c2eeabf
txtriii <twgaming56@gmail.com>
2025-03-26 11:18:32 +0100

    arrgghhh stressed
    finished shit
    added swing-based gui
    TODO GUI programmieren

--- Uebersetzung.java
+++ Uebersetzung.java
@@ -1,14 +1,50 @@
-public class Uebersetzung extends ComparableContent{
+public class Uebersetzung implements ComparableContent<Uebersetzung>{
     private String deutsch;
-    private String taubisch;
-    public Uebersetzung(String pDeutsch, String pTaubisch){
+    private TaubischString taubisch;
+    // private int taubischZahl;
+    private int iD;
+    public Uebersetzung(String pDeutsch, TaubischString pTaubisch, int pID){
+        this.deutsch = pDeutsch;
+        this.taubisch = pTaubisch;
+        this.iD = pID;
+        //this.taubischZahl = pTaubischZahl;
+    }
+    public Uebersetzung(String pDeutsch, TaubischString pTaubisch){
         this.deutsch = pDeutsch;
         this.taubisch = pTaubisch;
     }
+    public Uebersetzung(String pDeutsch){
+        this.deutsch = pDeutsch;
+    }
+   
     public String getDeutsch(){
-        return deutsch;
+        return this.deutsch;
     }
-    public String getTaubisch(){
-        return taubisch;
+    public String getTaubischString(){
+        return this.taubisch.getValue();
     }
+    public TaubischString getTaubischTString(){
+        return this.taubisch;
+    }
+    public int getID(){
+        return this.iD;
+    }
+    // public int getTaubischZahl(){
+    //     return this.taubischZahl;
+    // }
+    @Override
+    public boolean isEqual(Uebersetzung pContent) {
+        return this.deutsch.equals(pContent.getDeutsch());
+    }
+    @Override
+    public boolean isGreater(Uebersetzung pContent) {
+        return this.deutsch.compareTo(pContent.getDeutsch()) > 0;
+    }
+    @Override
+    public boolean isLess(Uebersetzung pContent) {
+        return this.deutsch.compareTo(pContent.getDeutsch()) < 0;
+    }
+
+
+
 }
